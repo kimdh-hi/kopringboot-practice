43b41a731f2002e953c4d69a5c73d24fbccee5ad
kimdh-hi <zbeld123@gmail.com>
2022-03-28 15:29:58 +0900

    querydsl 설정 추가

--- kotlin-springboot/build.gradle.kts
+++ kotlin-springboot/build.gradle.kts
@@ -1,13 +1,26 @@
-import org.jetbrains.kotlin.gradle.plugin.KotlinSourceSet
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+import org.jetbrains.kotlin.kapt3.base.Kapt.kapt
 
 plugins {
     id("org.springframework.boot") version "2.5.11"
     id("io.spring.dependency-management") version "1.0.11.RELEASE"
-    kotlin("jvm") version "1.5.32"
-    kotlin("plugin.spring") version "1.5.32"
-    kotlin("plugin.jpa") version "1.5.32"
+
+    val kotlinVersion = "1.5.32"
+    kotlin("jvm") version kotlinVersion
+    kotlin("plugin.spring") version kotlinVersion
+    kotlin("plugin.jpa") version kotlinVersion
+    kotlin("plugin.allopen") version kotlinVersion
+    kotlin("kapt") version kotlinVersion
 }
+
+allOpen {
+    annotation("javax.persistence.Entity")
+}
+
+noArg {
+    annotation("javax.persistence.Entity")
+}
+
 
 group = "com.ex"
 version = "0.0.1-SNAPSHOT"
@@ -17,12 +30,14 @@ repositories {
     mavenCentral()
 }
 
+val qeurydslVersion = "4.4.0"
+
 dependencies {
     implementation("org.springframework.boot:spring-boot-starter-data-jpa")
     implementation("org.springframework.boot:spring-boot-starter-validation")
     implementation("org.springframework.boot:spring-boot-starter-web")
 
-    //logging
+    // logging library
     implementation("io.github.microutils:kotlin-logging-jvm:2.0.11")
 
     implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
@@ -33,9 +48,14 @@ dependencies {
 //    runtimeOnly("org.postgresql:postgresql")
     testImplementation("org.springframework.boot:spring-boot-starter-test")
 
-    // mocking (mockk)
+    // mocking library (mockk)
     testImplementation("io.mockk:mockk:1.10.4")
     testImplementation("com.ninja-squad:springmockk:3.1.1")
+
+    // querydsl
+    implementation("com.querydsl:querydsl-jpa:$qeurydslVersion")
+    kapt("com.querydsl:querydsl-apt:$qeurydslVersion:jpa")
+    kapt("org.springframework.boot:spring-boot-configuration-processor")
 }
 
 tasks.withType<KotlinCompile> {
@@ -54,10 +74,10 @@ sourceSets {
         java {
             setSrcDirs(listOf("src/test/intg", "src/test/unit"))
         }
-
-        // deprecated  < 7.1
-//        withConvention(KotlinSourceSet::class) {
-//            kotlin.setSrcDirs(listOf("src/test/intg", "src/test/unit"))
-//        }
     }
+}
+
+kotlin.sourceSets.main {
+//    setBuildDir("$buildDir/generated/source/kapt/main")
+    setBuildDir("$buildDir")
 }
